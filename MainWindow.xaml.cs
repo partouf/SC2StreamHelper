3aa7990094e47600bc0a112b35eb6c7e489b081c
partouf <partouf@gmail.com>
2015-01-11 20:31:46 +0100

    First try

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace SC2StreamHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetWindow(IntPtr hWnd, GetWindow_Cmd uCmd);
+
+        enum GetWindow_Cmd : uint
+        {
+            GW_HWNDFIRST = 0,
+            GW_HWNDLAST = 1,
+            GW_HWNDNEXT = 2,
+            GW_HWNDPREV = 3,
+            GW_OWNER = 4,
+            GW_CHILD = 5,
+            GW_ENABLEDPOPUP = 6
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImportAttribute("User32.dll")]
+        private static extern IntPtr FindWindow(String ClassName, String WindowName);
+
+        private RemoteKeyboardInput input;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitBestOfPossibilities();
+            InitScorePossibilities();
+
+            input = new RemoteKeyboardInput();
+        }
+
+        private void InitBestOfPossibilities()
+        {
+            string[] arrPossibilities = new string[] {
+                "Bo1",
+                "Bo3",
+                "Bo5",
+                "Bo7",
+                "Bo9"
+            };
+
+            cbBestOf.ItemsSource = arrPossibilities;
+            cbBestOf.SelectedIndex = 1; // bo3 most common
+        }
+
+        private void InitScorePossibilities()
+        {
+            int[] arrPossibilities = new int[] {
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            };
+
+            cbPlayer1Score.ItemsSource = arrPossibilities;
+            cbPlayer2Score.ItemsSource = arrPossibilities;
+
+            cbPlayer1Score.SelectedIndex = 0;
+            cbPlayer2Score.SelectedIndex = 0;
+        }
+        
+        private void btnSendToGameHeart_Click(object sender, RoutedEventArgs e)
+        {
+            // swap to starcraft, 
+            IntPtr hWnd = FindWindow(null, "StarCraft II");
+            if (hWnd != null)
+            {
+                SetForegroundWindow(hWnd); //Activate it
+            }
+            else
+            {
+                MessageBox.Show("StarCraft II Not Found!");
+                return;
+            }
+
+            // enable score if more than Bo1
+            if (cbBestOf.SelectedIndex > 0)
+            {
+                input.Send_CtrlShiftKeyPress((byte)'S');
+
+                // set best of...
+                input.Send_CtrlShiftKeyPress((short)(0x30 + cbBestOf.SelectedIndex + 1));
+            }
+
+            // swap player positions
+            if (chkDifferentSpawns.IsChecked.GetValueOrDefault(false))
+            {
+                input.Send_CtrlKeyPress((byte)'X');
+
+                // note: players are swapped, add scores reversed
+
+                // 1st player
+                if (cbPlayer2Score.SelectedIndex > 0)
+                {
+                    input.Send_ShiftKeyPress((short)(0x30 + cbPlayer2Score.SelectedIndex));
+                }
+
+                // 2nd player
+                if (cbPlayer1Score.SelectedIndex > 0)
+                {
+                    input.Send_CtrlKeyPress((short)(0x30 + cbPlayer1Score.SelectedIndex));
+                }
+            }
+            else
+            {
+                // 1st player
+                if (cbPlayer1Score.SelectedIndex > 0)
+                {
+                    input.Send_ShiftKeyPress((short)(0x30 + cbPlayer1Score.SelectedIndex));
+                }
+
+                // 2nd player
+                if (cbPlayer2Score.SelectedIndex > 0)
+                {
+                    input.Send_CtrlKeyPress((short)(0x30 + cbPlayer2Score.SelectedIndex));
+                }
+            }
+
+            // open production tab
+            input.Send_NormalKeyPress((byte)'D');
+        }
+    }
+}
---
+++ RemoteKeyboardInput.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC2StreamHelper
+{
+    class RemoteKeyboardInput
+    {
+        //  source: http://www.codeproject.com/Questions/279641/c-sharp-DirectInput-Send-Key
+
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+        public const int KEYEVENTF_KEYDOWN = 0x0000;
+        public const int KEYEVENTF_KEYUP = 0x0002;
+        public const int KEYEVENTF_UNICODE = 0x0004;
+        public const int KEYEVENTF_SCANCODE = 0x0008;
+
+        public const short VK_SHIFT = 0x10;
+        public const short VK_CONTROL = 0x11;
+
+
+        public void Send_SingleKey(short Keycode, int KeyUporDown)
+        {
+            INPUT[] InputData = new INPUT[1];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = Keycode;
+            InputData[0].ki.dwFlags = KeyUporDown;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.wVk = 0;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(1, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        public void Send_NormalKeyPress(short Keycode)
+        {
+            INPUT[] InputData = new INPUT[2];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = 0;
+            InputData[0].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.wVk = Keycode;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[1].type = 1;
+            InputData[1].ki.wScan = 0;
+            InputData[1].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[1].ki.time = 0;
+            InputData[1].ki.wVk = Keycode;
+            InputData[1].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(2, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        public void Send_CtrlKeyPress(short Keycode)
+        {
+            uint iNumberOfInputs = 4;
+            INPUT[] InputData = new INPUT[iNumberOfInputs];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = 0;
+            InputData[0].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.wVk = VK_CONTROL;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[1].type = 1;
+            InputData[1].ki.wScan = 0;
+            InputData[1].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[1].ki.time = 0;
+            InputData[1].ki.wVk = Keycode;
+            InputData[1].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[2].type = 1;
+            InputData[2].ki.wScan = 0;
+            InputData[2].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[2].ki.time = 0;
+            InputData[2].ki.wVk = Keycode;
+            InputData[2].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[3].type = 1;
+            InputData[3].ki.wScan = 0;
+            InputData[3].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[3].ki.time = 0;
+            InputData[3].ki.wVk = VK_CONTROL;
+            InputData[3].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(iNumberOfInputs, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        public void Send_ShiftKeyPress(short Keycode)
+        {
+            uint iNumberOfInputs = 4;
+            INPUT[] InputData = new INPUT[iNumberOfInputs];
+
+            InputData[0].type = 1;
+            InputData[0].ki.wScan = 0;
+            InputData[0].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[0].ki.time = 0;
+            InputData[0].ki.wVk = VK_SHIFT;
+            InputData[0].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[1].type = 1;
+            InputData[1].ki.wScan = 0;
+            InputData[1].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[1].ki.time = 0;
+            InputData[1].ki.wVk = Keycode;
+            InputData[1].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[2].type = 1;
+            InputData[2].ki.wScan = 0;
+            InputData[2].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[2].ki.time = 0;
+            InputData[2].ki.wVk = Keycode;
+            InputData[2].ki.dwExtraInfo = IntPtr.Zero;
+
+            InputData[3].type = 1;
+            InputData[3].ki.wScan = 0;
+            InputData[3].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[3].ki.time = 0;
+            InputData[3].ki.wVk = VK_SHIFT;
+            InputData[3].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(iNumberOfInputs, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        public void Send_CtrlShiftKeyPress(short VKCode)
+        {
+            uint iNumberOfInputs = 6;
+            uint idx = 0;
+            INPUT[] InputData = new INPUT[iNumberOfInputs];
+
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VK_CONTROL;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            idx++;
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VK_SHIFT;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            idx++;
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYDOWN;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VKCode;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            idx++;
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VKCode;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            idx++;
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VK_SHIFT;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            idx++;
+            InputData[idx].type = 1;
+            InputData[idx].ki.wScan = 0;
+            InputData[idx].ki.dwFlags = KEYEVENTF_KEYUP;
+            InputData[idx].ki.time = 0;
+            InputData[idx].ki.wVk = VK_CONTROL;
+            InputData[idx].ki.dwExtraInfo = IntPtr.Zero;
+
+            SendInput(iNumberOfInputs, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+    }
+}
